f3f496acc32f1d732c47f03bb208eade67c78ce8
Anna <annatos3006@mail.ru>
2025-11-11 22:16:00 +0300

    Implement bonus calculation

--- src/main.java
+++ src/main.java
@@ -1,2 +1,18 @@
 public class main {
+    public static void main(String[] args) {
+        // Исходные данные
+        int initialBalance = 100;  // начальный счёт клиента
+        int refillAmount = 1100;   // сумма пополнения
+
+        // Логика расчёта
+        int bonus = 0;
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100;
+        }
+        int finalBalance = initialBalance + refillAmount + bonus;
+
+        // Вывод результата
+        System.out.println("Итоговый счёт: " + finalBalance);
+        System.out.println("Бонусные рубли: " + bonus);
+    }
 }
